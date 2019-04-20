41e1b30423add6a8107e8454983ef51e2012fc5d
unknown <cathaldnnll@yahoo.com>
2019-04-20 16:54:23 +0100

    Created initial models and ef migration

---
+++ ToDoList/Models/Task.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class Task
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Title is required")]
+        [StringLength(75)]
+        public string Title { get; set; }
+
+        [Required(ErrorMessage = "Description is required")]
+        [StringLength(255)]
+        public string Description { get; set; }
+
+        public bool IsTaskComplete { get; set; }
+
+        [Display(Name = "Last Updated")]
+        public DateTime? LastUpdated { get; set; }
+
+        [Display(Name = "Created")]
+        public DateTime RecordCreated { get; set; }
+
+        public Task()
+        {
+            IsTaskComplete = false;
+            RecordCreated = DateTime.Now;
+        }
+    }
+}
---
+++ ToDoList/Models/TaskUpdate.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace ToDoList.Models
+{
+    public class TaskUpdate
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int TaskId { get; set; }
+       
+        public string UpdateText { get; set; }
+
+        public DateTime RecordCreated { get; set; }
+
+        public TaskUpdate()
+        {
+            RecordCreated = DateTime.Now;
+        }
+    }
+}
