25e5db938bbb1f6cd9e0d2284ac68973b8b6b21f
unknown <cathaldnnll@yahoo.com>
2019-04-22 14:23:39 +0100

    Unit Tests

---
+++ ToDoListTests/TaskControllerTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Mvc;
+using ToDoList;
+using ToDoList.Controllers;
+using ToDoList.Models;
+using ToDoList.ViewModels;
+using ToDoList.DAL.Services;
+using ToDoList.DAL.Interfaces;
+
+namespace ToDoListTests
+{
+    [TestClass]
+    public class TaskControllerTest
+    {
+        [TestMethod]
+        public void Index()
+        {
+            TaskController taskController = new TaskController();
+
+            ActionResult result = taskController.Index() as ActionResult;
+
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod]
+        public void Detail()
+        {
+            TaskController taskController = new TaskController();
+
+            ActionResult result = taskController.Detail(5) as ActionResult;
+
+            Assert.IsNotNull(result);
+        }
+    }
+}
